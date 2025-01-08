b9e3a7f23b15679eb5ef73152f1225285ade0c37
Tommysons <lokatorist2@inbox.lv>
2025-01-07 22:49:02 -0800

    Header and footer

---
+++ lib/constats.ts
@@ -0,0 +1,5 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Online Store'
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || 'Buy once, Buy always'
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'An Online shop for your needs.'
