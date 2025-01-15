854adf5051a19bfd4343dce062c61ddb40fbfcb0
Tommysons <lokatorist2@inbox.lv>
2025-01-14 22:44:11 -0800

    Sign in

--- lib/constats.ts
+++ lib/constats.ts
@@ -1,8 +1,14 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Online Store'
+
 export const APP_SLOGAN =
   process.env.NEXT_PUBLIC_APP_SLOGAN || 'Buy once, Buy always'
+
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'An Online shop for your needs.'
+
+export const APP_COPYRIGHT =
+  process.env.NEXT_PUBLIC_APP_COPYRIGHT ||
+  `Copyright © 2025 ${APP_NAME}. All rights reserved.`
 
 export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)
 
